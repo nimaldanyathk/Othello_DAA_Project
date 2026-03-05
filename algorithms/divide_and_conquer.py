b98fb3e958ee6ccfeb4415edcec43f91311d1130
Rohan7511 <66904576+Rohan7511@users.noreply.github.com>
2026-03-06 03:29:44 +0530

    Implemented new divide and conquer strategy

--- algorithms/divide_and_conquer.py
+++ algorithms/divide_and_conquer.py
@@ -1,128 +1,123 @@
 from model.board import Board
-from algorithms.heuristics import weighted_heuristic
-from model.game_state import GameState 
+from model.game_state import GameState
 
-def evaluatemove(board, moves, depth, playerturn, rootplayer, ismax):
-    if len(moves) > 1:
-        mid = len(moves) // 2
-        leftmoves = moves[:mid]
-        rightmoves = moves[mid:]
-        leftresults = evaluatemove(board, leftmoves, depth, playerturn, rootplayer, ismax)
-        rightresults = evaluatemove(board, rightmoves, depth, playerturn, rootplayer, ismax)
-        
-        return leftresults + rightresults
+def classifier(board, moves):
+    A = [] 
+    B = []  
+    C = []  
+    D = []  
+    size = len(board.grid)
+    corners = [(0,0),(0,size-1),(size-1,0),(size-1,size-1)]
+    cedges = [
+        (0,1),(1,0),(1,1),
+        (0,size-2),(1,size-2),(1,size-1),
+        (size-2,0),(size-2,1),(size-1,1),
+        (size-2,size-1),(size-1,size-2),(size-2,size-2)
+    ]
 
-    move = moves[0]
-    newboard, _ = board.apply_move(move[0], move[1], playerturn)
+    for r,c in moves:
+        if (r,c) in corners:
+            A.append((r,c))
+        elif (r,c) in cedges:
+            D.append((r,c))
+        elif r==0 or r==size-1 or c==0 or c==size-1:
+            B.append((r,c))
+        else:
+            C.append((r,c))
 
-    if depth == 0 or newboard.is_full():
-        score = weighted_heuristic(newboard, rootplayer)
-        return [(score, move)]
-    opponent = -playerturn
-    opponentmoves = newboard.get_valid_moves(opponent)
-    if not opponentmoves:
-        score = weighted_heuristic(newboard, rootplayer)
-        return [(score, move)]
-    opponentresults = evaluatemove(newboard, opponentmoves, depth - 1, opponent, rootplayer, not ismax)
-    opponentscores = [s for s, m in opponentresults]
-    if ismax:
-        bestscore = min(opponentscores)
-    else:
-        bestscore = max(opponentscores)
-    return [(bestscore, move)]
+    return A,B,C,D
 
-def evaluatemovevisual(board, moves, depth, playerturn, rootplayer, ismax):
-    viewstate = GameState(board, playerturn)
-    yield {'type': 'search_node', 'state': viewstate, 'depth': depth}
-    
-    if len(moves) > 1:
-        mid = len(moves) // 2
-        leftmoves = moves[:mid]
-        rightmoves = moves[mid:]
-        
-        leftresults = yield from evaluatemovevisual(board, leftmoves, depth, playerturn, rootplayer, ismax)
-        rightresults = yield from evaluatemovevisual(board, rightmoves, depth, playerturn, rootplayer, ismax)
-        
-        return leftresults + rightresults
-    move = moves[0]
-    newboard, _ = board.apply_move(move[0], move[1], playerturn)
-    if depth == 0 or newboard.is_full():
-        score = weighted_heuristic(newboard, rootplayer)
-        leafstate = GameState(newboard, -playerturn)
-        yield {'type': 'leaf', 'state': leafstate, 'depth': depth, 'score': score}
-        return [(score, move)]
-        
-    opponent = -playerturn
-    opponentmoves = newboard.get_valid_moves(opponent)
-    if not opponentmoves:
-        score = weighted_heuristic(newboard, rootplayer)
-        leafstate = GameState(newboard, -playerturn)
-        yield {'type': 'leaf', 'state': leafstate, 'depth': depth, 'score': score}
-        return [(score, move)]
-    opponentresults = yield from evaluatemovevisual(newboard, opponentmoves, depth - 1, opponent, rootplayer, not ismax)
-    opponentscores = [s for s, m in opponentresults]
-    if ismax:
-        bestscore = min(opponentscores)
-    else:
-        bestscore = max(opponentscores)
-    return [(bestscore, move)]
 
-def choosebestmove(board, player, depth=3):
-    moves = board.get_valid_moves(player)
-    if not moves:
-        return None
-    scoredmoves = evaluatemove(board, moves, depth, player, player, ismax=True)
-    
-    def mergesort(arr):
-        if len(arr) <= 1:
-            return arr
+def quaddom(board, player, r0, c0, size):
 
-        mid = len(arr) // 2
-        left = mergesort(arr[:mid])
-        right = mergesort(arr[mid:])
+    if size <= 2:
+        cpu = 0
+        human = 0
 
-        return merge(left, right)
-    def merge(left, right):
-        result = []
-        i = j = 0
+        for r in range(r0, min(r0 + size, len(board.grid))):
+            for c in range(c0, min(c0 + size, len(board.grid))):
 
-        while i < len(left) and j < len(right):
-            if left[i][0] > right[j][0]:
-                result.append(left[i])
-                i += 1
-            else:
-                result.append(right[j])
-                j += 1
+                if board.grid[r][c] == player:
+                    cpu += 1
+                elif board.grid[r][c] == -player:
+                    human += 1
 
-        result.extend(left[i:])
-        result.extend(right[j:])
-        return result
-    scoredmoves = mergesort(scoredmoves)
-    if not scoredmoves:
-        return None        
-    bestscore, bestmove = scoredmoves[0]
-    return bestmove
+        return 1 if cpu > human else 0
 
-def choosebestmovevisual(board, player, depth=3):
+    half = size // 2
+
+    q1 = quaddom(board, player, r0, c0, half)
+    q2 = quaddom(board, player, r0, c0 + half, half)
+    q3 = quaddom(board, player, r0 + half, c0, half)
+    q4 = quaddom(board, player, r0 + half, c0 + half, half)
+
+    return q1 + q2 + q3 + q4
+
+def evalmove(board, move, player):
+    newboard,_ = board.apply_move(move[0], move[1], player)
+    size = len(newboard.grid)
+    res = quaddom(newboard, player, 0, 0, size)
+    score = res
+    return score
+
+def choosebestmove(board, player):
 
     moves = board.get_valid_moves(player)
-    
+    if not moves:
+        return None  
+
+    A,B,C,D = classifier(board, moves)
+    if A:
+        bucket = A
+    elif B:
+        bucket = B
+    elif C:
+        bucket = C
+    else:
+        bucket = D
+
+    bestm = None
+    bests = -1
+    for move in bucket:
+        score = evalmove(board, move, player)
+        if score > bests:
+            bests = score
+            bestm = move
+    return bestm
+
+def choosebestmovevisual(board, player):
+    moves = board.get_valid_moves(player)
     if not moves:
         return
-    
-    scoredmoves = yield from evaluatemovevisual(board, moves, depth, player, player, ismax=True)
-    
-    scoredmoves.sort(key=lambda x: x[0], reverse=True)
-    
-    if not scoredmoves:
-        return
-        
-    bestscore, bestmove = scoredmoves[0]
-    newboard, _ = board.apply_move(bestmove[0], bestmove[1], player)
-    
-    from model.game_state import GameState
-    nextplayer = -player
-    resultstate = GameState(newboard, nextplayer)
-    
-    yield {'type': 'result', 'state': resultstate}
 
+    A,B,C,D = classifier(board, moves)
+    if A:
+        bucket = A
+    elif B:
+        bucket = B
+    elif C:
+        bucket = C
+    else:
+        bucket = D
+    bestmove = None
+    bestscore = -1
+
+
+    for move in bucket:
+
+        newboard,_ = board.apply_move(move[0], move[1], player)
+
+        viewstate = GameState(newboard, -player)
+
+        yield {'type':'search_node','state':viewstate}
+
+        score = evalmove(board, move, player)
+
+        if score > bestscore:
+            bestscore = score
+            bestmove = move
+
+
+    newboard,_ = board.apply_move(bestmove[0], bestmove[1], player)
+    resultstate = GameState(newboard, -player)
+    yield {'type':'result','state':resultstate}
